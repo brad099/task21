fa4309c0377cfe689afa30b2c4f55664973bb096
B_r_A_d <brad990124@gmail.com>
2022-10-27 18:02:58 +0400

    patch3

--- Task2/Assets/Scripts/PlayerController.cs
+++ Task2/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using Dreamteck.Splines;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
-
+using Cinemachine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float Speed;
@@ -30,9 +30,12 @@ public class PlayerController : MonoBehaviour
     Collider[] colliders;
     SplineFollower _splineFollower;
     Animator anim;
-    Vector3 distance ;
-
-    
+    [SerializeField]List <Transform> droppos;
+    Vector3 distance;
+    int dropCountt = 0;
+    Vector3 sellDistance = new Vector3(1, 0, 0);
+    private CinemachineVirtualCamera _cinemachineCamera;
+    private CinemachineTransposer _transposer;
     
     // Getting Components
     void Start()
@@ -40,6 +43,8 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
         _splineFollower = GetComponentInParent<SplineFollower>();
+        _cinemachineCamera = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+        _transposer = _cinemachineCamera.GetCinemachineComponent<CinemachineTransposer>();
     }
 
     // Moving and Jump
@@ -83,11 +88,17 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-                //Dropping items to base
+                //Dropping items to base // Finish Line
          public void OnTriggerStay(Collider other)
          {
             if (other.transform.CompareTag("Dropzone"))
-        {
+            {
+                //Rotation on Death
+                 transform.DOLocalRotate(new Vector3(0,-180,0),0.3f);    
+                _transposer.m_BindingMode = CinemachineTransposer.BindingMode.LockToTargetOnAssign;
+                _transposer.m_FollowOffset = new Vector3(10f,7f,1.5f);
+
+                //Drop
                 if (Time.time >= NextDropTime)
                 {
                     if (CollectedItems.Count > 0)
@@ -95,10 +106,17 @@ public class PlayerController : MonoBehaviour
                      GameObject go = CollectedItems[CollectedItems.Count - 1];
                      go.transform.parent = null;
                      var Seq = DOTween.Sequence();
-                     Seq.Append(go.transform.DOJump(dropTransform.position + new Vector3((dropCount * dropDistanceBetween),0, 0), 2, 1, 0.3f))
+                     Seq.Append(go.transform.DOJump((droppos[dropCountt].transform.position + sellDistance), 2, 1, 0.3f))
                     .Join(go.transform.DOScale(1.2f, 0.1f))
                     .Insert(0.1f, go.transform.DOScale(1, 0.2f))
                     .AppendCallback(() => { go.transform.rotation = Quaternion.Euler(0, 0, 0); });
+                    dropCountt ++;
+
+                    if (dropCountt == 3)
+                    {
+                        dropCountt = 0;
+                        sellDistance.x +=1;
+                    }
                      CollectedItems.Remove(go);
                      dropCount++;
                      NextDropTime = Time.time + DropSecond / DropRate;
@@ -130,8 +148,6 @@ public class PlayerController : MonoBehaviour
         {
             anim.SetBool("GroundT",true);
             _isGround=true;
-            // Transform child = transform.GetChild(0);
-            // child.localRotation = Quaternion.identity;
         }
     }
         // Restarting
@@ -140,15 +156,16 @@ public class PlayerController : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-
         //Death Drop things
-    void dropItems (){
+    void dropItems ()
+    {
         foreach (GameObject drops in CollectedItems)
         {
             distance = new Vector3 (Random.Range(-1, 3), 0.5f, Random.Range(-1, 3));
             drops.transform.parent = null;
-             var seq = DOTween.Sequence();
-             seq.Append(drops.transform.DOLocalJump((gameObject.transform.position + distance), 1.3f, 1, 0.5f)).Join(drops.transform.DOScale(1f, 0.2f));
+            var seq = DOTween.Sequence();
+            drops.AddComponent<Rigidbody>();
+            seq.Append(drops.transform.DOLocalJump((gameObject.transform.position + distance), 1.3f, 1, 0.5f)).Join(drops.transform.DOScale(1f, 0.2f));
              CollectedItems.Remove(drops.gameObject);
         } 
     }
